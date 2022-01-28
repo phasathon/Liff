03c98ad2ed1b1b6e4cbf8143f34992363b5efc95
PHASATHON-Y\Phasathon <phasathon.yingcharoenrat@stream.co.th>
2022-01-28 12:41:22 +0700

    no message

--- register.js
+++ register.js
@@ -1,10 +1,9 @@
 window.onload = function (e) {
     liff.init(function (data) {
-        window.alert(JSON.stringify(data));
         initializeApp(data);
     });
 };
 
 function initializeApp(data) {
-    document.getElementById('line_user_id').textContent = data.context.userId;
+    document.getElementById('line_user_id').value = "data.context.userId";
 }
