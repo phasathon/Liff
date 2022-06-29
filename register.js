4a80f8369b90bd01ac4c533f4cfbbe41cf58446d
PHASATHON-Y\Phasathon <phasathon.yingcharoenrat@stream.co.th>
2022-06-29 12:02:52 +0700

    no message

--- register.js
+++ register.js
@@ -5,6 +5,5 @@ window.onload = function (e) {
 };
 
 function initializeApp(data) {
-    window.alert("line_user_id: " + data.context.userId);
-    document.getElementById('line_user_id').value = data.context.userId;
+    $('input[name="line_user_id"]').val(profile.userId);
 }
